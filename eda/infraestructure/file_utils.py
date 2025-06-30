e4719a8928e1367f1379405c2e9ad76aee084ccf
Paloma Mesón de Arana <pmeson.da@gmail.com>
2025-06-30 20:44:20 +0200

    file_utils working

--- eda/infraestructure/file_utils.py
+++ eda/infraestructure/file_utils.py
@@ -2,31 +2,54 @@
 from pathlib import Path
 import pandas as pd
 import csv
+import logging
 
+logger = logging.getLogger(__name__)
 
 # FUNCTIONS
 
 # Open file
 def detectFileExt(file_name, sheet=None):
-    path = Path( '../../files/input')
-    final_path = path / file_name
+    base_path = Path(__file__).resolve().parents[1]
+    final_path = base_path / 'files' / 'input' / file_name
     file_ext = file_name.split('.')[1]
+
     try:
         if file_ext =='csv':
             return pd.read_csv(final_path)
         if file_ext == 'xlsx':
             return  pd.read_excel(final_path,sheet)
         else:
-            return print("File extension not supported")
+            logger.info("File extension not supported")
+            return None 
     except FileNotFoundError:
-        print('No file found')
+        logger.error('No file found')
+
+# Open file
+def detectFileExt_debug(file_name, sheet=None):
+    base_path = Path(__file__).resolve().parents[2]
+    final_path = base_path / 'files' / 'input' / file_name
+    file_ext = file_name.split('.')[1]
+
+    try:
+        if file_ext =='csv':
+            return pd.read_csv(final_path)
+        if file_ext == 'xlsx':
+            print(f'Path: {final_path}')
+            return  pd.read_excel(final_path,sheet)
+        else:
+            logger.info("File extension not supported")
+            return None 
+    except FileNotFoundError:
+        print(f'Path: {final_path}')
+        logger.error('No file found')
 
 
 # Testing
 test_file = 'customer-details.xlsx'
-test_sheet = 2012
-df = detectFileExt(test_file, test_sheet)
-print(df.head())
+test_sheet = '2012'
+df = detectFileExt_debug(test_file,test_sheet)
+print(df)
 
 
 
--- eda/main.py
+++ eda/main.py
@@ -1,1 +1,21 @@
-# Import libraries
+# Import Libraries
+from pathlib import Path
+import pandas as pd
+import csv
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+def main():
+    base_path = Path(__file__).resolve().parents[1]
+    path_bank =base_path.joinpath('files/input/bank-additional.csv')
+    
+    df_bank = pd.read_csv(path_bank)
+    return df_bank.head()
+
+
+
+execute = main()
+print(execute)
+
