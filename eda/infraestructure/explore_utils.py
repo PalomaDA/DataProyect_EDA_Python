1eea43512aedef545a8860df5a9d7e7a84509ed6
Paloma Mesón de Arana <pmeson.da@gmail.com>
2025-08-21 13:37:24 +0200

    advance cleaning

--- eda/infraestructure/explore_utils.py
+++ eda/infraestructure/explore_utils.py
@@ -37,3 +37,20 @@ def info_dataframe(df):
     info_df = pd.DataFrame(column_info)
 
     return info_df
+
+def column_type_dataframe(df, col_type):
+    """
+    Returns a filtered dataframe that only shows the columns of a defined type (object, float, int, date...) and a list with the columns of that data type.
+
+    Args:
+        df (datafrane): Original dataframe
+        col_type (string): The desired data type of the columns
+
+    Returns:
+        _type_: A filtered dataframe and the columns of that type (for making changes in original df)
+    """
+    df_info = info_dataframe(df)
+    df_filtered = df_info[df_info["DTYPE"] == col_type]
+    type_cols = [x for x in df_filtered["COLUMN_NAME"]]
+    df_type = df[type_cols]
+    return df_type, type_cols
